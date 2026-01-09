35bfd084a58f68b250dec1b05f4f402351b01fe4
Upama Prapti <cpcourse9806@gmail.com>
2026-01-09 11:26:10 +0600

    Update update.php

--- views/user/update.php
+++ views/user/update.php
@@ -19,11 +19,23 @@ if(!isset($user)) $user = ['name' => '', 'email' => ''];
 <section class="form-container">
    <form action="index.php?route=update" method="post">
       <h3>update profile</h3>
+      
+      <?php
+      if(!empty($message)){
+         foreach($message as $msg){
+            $bg_color = (strpos($msg, 'successfully') !== false) ? '#28a745' : '#dc3545';
+            echo '<div class="message" style="background: '.$bg_color.'; color: white; padding: 1rem; border-radius: 0.5rem; margin-bottom: 1rem; text-align: center;">
+               <span>'.$msg.'</span>
+            </div>';
+         }
+      }
+      ?>
+      
       <input type="text" name="name" placeholder="<?= $user['name']; ?>" class="box" maxlength="50">
       <input type="email" name="email" placeholder="<?= $user['email']; ?>" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
       <input type="password" name="old_pass" placeholder="enter your old password" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
-      <input type="password" name="new_pass" placeholder="enter your new password" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
-      <input type="password" name="confirm_pass" placeholder="confirm your new password" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="password" name="new_pass" placeholder="new password (min 8 characters)" class="box" maxlength="50" minlength="8" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="password" name="confirm_pass" placeholder="confirm your new password" class="box" maxlength="50" minlength="8" oninput="this.value = this.value.replace(/\s/g, '')">
       <input type="submit" value="update now" name="submit" class="btn">
    </form>
 </section>
@@ -32,4 +44,3 @@ if(!isset($user)) $user = ['name' => '', 'email' => ''];
 <script src="<?php echo ASSETS_URL; ?>js/script.js"></script>
 </body>
 </html>
-
