31553b32999f96b7001b6011561a4f5667251cde
Upama Prapti <cpcourse9806@gmail.com>
2026-01-09 11:08:50 +0600

    Rename method to follow naming conventions

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -1,10 +1,7 @@
 <?php
 require_once __DIR__ . '/../config/config.php';
 
-class CategoryController {
-    public function allCategories() {
-        include __DIR__ . '/../views/user/all_category.php';
-    }
+function category_all_categories() {
+    include __DIR__ . '/../views/user/all_category.php';
 }
 ?>
-
