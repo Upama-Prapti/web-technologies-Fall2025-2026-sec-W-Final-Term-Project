146ceaa1cfdda85b1ad60f4b7a27fcbfe7373b53
Upama Prapti <cpcourse9806@gmail.com>
2026-01-19 01:05:03 +0600

    Update create_post.php

--- views/user/create_post.php
+++ views/user/create_post.php
@@ -9,7 +9,6 @@ if(!isset($user)) $user = ['name' => ''];
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Create Post</title>
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
    <link rel="stylesheet" href="<?php echo ASSETS_URL; ?>css/style.css">
 </head>
 <body>
