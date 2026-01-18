3036509b03190e502cb30ed18c50de9722a01e33
Upama Prapti <cpcourse9806@gmail.com>
2026-01-19 01:01:41 +0600

    Update users_accounts.php

--- views/admin/users_accounts.php
+++ views/admin/users_accounts.php
@@ -9,7 +9,6 @@ if(!isset($users)) $users = [];
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Users Accounts</title>
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
    <link rel="stylesheet" href="<?php echo ASSETS_URL; ?>css/admin_style.css">
 </head>
 <body>
