4db4126223eda43ee62ad09d69b6e12c82638175
Upama Prapti <cpcourse9806@gmail.com>
2026-01-19 00:58:03 +0600

    Update comments.php

--- views/admin/comments.php
+++ views/admin/comments.php
@@ -9,7 +9,6 @@ if(!isset($comments)) $comments = [];
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Comments</title>
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
    <link rel="stylesheet" href="<?php echo ASSETS_URL; ?>css/admin_style.css">
 </head>
 <body>
@@ -29,7 +28,7 @@ if(!isset($comments)) $comments = [];
    <?php } ?>
    <div class="box">
       <div class="user">
-         <i class="fas fa-user"></i>
+         <span style="font-weight: bold;">👤</span>
          <div class="user-info">
             <span><?= $fetch_comments['user_name']; ?></span>
             <div><?= $fetch_comments['date']; ?></div>
