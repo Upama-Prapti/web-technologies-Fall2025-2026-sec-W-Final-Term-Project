82966e58847cbca4754bf0221ef215b139db8fb7
Upama Prapti <cpcourse9806@gmail.com>
2026-01-19 01:02:38 +0600

    Update all_category.php

--- views/user/all_category.php
+++ views/user/all_category.php
@@ -5,7 +5,6 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>All Categories</title>
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
    <link rel="stylesheet" href="<?php echo ASSETS_URL; ?>css/style.css">
 </head>
 <body>
